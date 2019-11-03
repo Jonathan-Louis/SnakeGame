111851b187965002ba60a827d2aa9005193e5269
Jonathan Louis <jonathan.louis@snhu.edu>
2019-11-03 09:15:56 -0500

    Initial Commit

---
+++ SnakeGame/src/MainGame/RenderPanel.java
@@ -0,0 +1,58 @@
+package MainGame;
+
+import java.awt.*;
+
+import javax.swing.JPanel;
+
+public class RenderPanel  extends JPanel{
+
+	protected void paintComponent(Graphics g) {
+		
+		//intialize render panel
+		super.paintComponent(g);
+		
+		//set background
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, 760, 510);
+		
+		Snake snake = Snake.snake;
+		
+		//draw snake body
+		g.setColor(Color.GREEN);
+		if(!snake.snakeBody.isEmpty()) {
+			for(Point point : snake.snakeBody) {
+				g.fillRect(point.x * snake.SCALE, point.y * snake.SCALE, snake.SCALE, snake.SCALE);
+			}
+		}
+		
+		//draw snake head
+		g.setColor(Color.ORANGE);
+		g.fillRect(snake.head.x * snake.SCALE, snake.head.y * snake.SCALE, snake.SCALE, snake.SCALE);
+		
+		//draw apple
+		g.setColor(Color.RED);
+		g.fillRect(snake.apple.x * snake.SCALE, snake.apple.y * snake.SCALE, snake.SCALE, snake.SCALE);
+		
+		//draw score line
+		String scoreLine = "Score: " + snake.score;
+		g.setColor(Color.WHITE);
+		g.drawString(scoreLine, (int)(getWidth() / 2 - scoreLine.length() * 2.5f), 10);
+		
+		//draw high score line
+		String highScoreLine = "High Score: " + snake.highScoreString;
+		g.setColor(Color.WHITE);
+		g.drawString(highScoreLine, (int)(getWidth() - highScoreLine.length() * 7.5f), 10);
+		
+		//draw game over when game ends
+		if(snake.paused) {
+			String pausedString = "Paused -- press space to continue";
+			g.drawString(pausedString, (int) (snake.WIDTH / 2 - pausedString.length() * 2.5f), (int) snake.HEIGHT / 2);
+		}
+		
+		//draw game over when game ends
+		if(snake.gameOver) {
+			String gameOver = "Game Over!";
+			g.drawString(gameOver, (int) getWidth() / 2 - gameOver.length(), (int) getHeight() / 2);
+		}
+	}
+}
---
+++ SnakeGame/src/MainGame/Snake.java
@@ -0,0 +1,289 @@
+package MainGame;
+
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.ArrayList;
+import java.util.Random;
+import java.io.*;
+import java.util.Scanner;
+
+
+import javax.swing.JFrame;
+import javax.swing.Timer;
+
+
+
+
+public class Snake implements ActionListener, KeyListener{
+	
+	public static Snake snake;
+	
+	public static final int WIDTH = 750, HEIGHT = 500;
+	
+	public static final int RIGHT = 0, LEFT = 1, UP = 2, DOWN = 3, SCALE = 10;
+	
+	public int direction, score, tail, highScore;
+	
+	public int time, ticks = 0, delay, initialDelay = 50;
+	
+	public boolean overlapping;
+	
+	public String highScoreString;
+	
+	public JFrame frame;
+	
+	public Timer timer = new Timer(initialDelay, this);
+	
+	public RenderPanel renderPanel;
+	
+	public ArrayList<Point> snakeBody = new ArrayList();
+	
+	public Point apple, head;
+	
+	public Random random;
+	
+	public boolean gameOver = false, paused = false;
+	
+	public Dimension dimension;
+	
+	
+	public Snake() {
+		//get screen info
+		dimension = Toolkit.getDefaultToolkit().getScreenSize();
+		frame = new JFrame("Snake Game");
+		frame.setTitle("Snake Game");
+		
+		//set frame size to show all game
+		frame.setSize(WIDTH + 26, HEIGHT + 49);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		
+		//center frame on screen
+		frame.setLocation(dimension.width / 2 - WIDTH / 2, dimension.height / 2 - HEIGHT / 2);
+		
+		//add render panel to draw images to frame
+		frame.add(renderPanel = new RenderPanel());
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		//add key listener for controls
+		frame.addKeyListener((KeyListener) this);
+		
+		//start the game
+		startGame();
+	}
+	
+	//initialize the game
+	public void startGame() {
+		score = 0;
+		tail = 1;
+		time = 0;
+		head = new Point(10, 10);
+		random = new Random();
+		snakeBody.clear();
+		direction = DOWN;
+		timer.setDelay(initialDelay);
+		delay = initialDelay;
+		ticks = 0;
+		
+		//get high score string
+		getHighScore();
+		Scanner sc = new Scanner(highScoreString);
+			highScore = sc.nextInt();
+		sc.close();
+		
+		//set first apple
+		apple = new Point(random.nextInt(WIDTH / SCALE), random.nextInt(HEIGHT / SCALE));
+		
+		//start timer for JPanel
+		timer.start();
+	}
+	
+	//snake movement control
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		renderPanel.repaint();
+		ticks++;
+		if(!gameOver && !paused && ticks % 2 == 0) {
+			time++;
+			snakeBody.add(new Point(head.x, head.y));
+			
+			if(direction == UP) {
+				if(tailCheck(head.x, head.y - 1) && head.y - 1 >=0) {
+					head = new Point(head.x, head.y - 1);
+				}
+				else {
+					gameOver = true;
+				}
+			}
+			
+			if(direction == DOWN) {
+				if(tailCheck(head.x, head.y + 1) && head.y + 1 <= (HEIGHT / SCALE)) {
+					head = new Point(head.x, head.y + 1);
+				}
+				else {
+					gameOver = true;
+				}
+			}
+			
+			if(direction == RIGHT) {
+				if(tailCheck(head.x + 1, head.y) && head.x + 1 <= (WIDTH / SCALE)) {
+					head = new Point(head.x + 1, head.y);
+				}
+				else {
+					gameOver = true;
+				}
+			}
+			
+			if(direction == LEFT) {
+				if(tailCheck(head.x - 1, head.y) && head.x - 1 >= 0) {
+					head = new Point(head.x - 1, head.y);
+				}
+				else {
+					gameOver = true;
+				}
+			}
+			
+			//remove last snake body with movement
+			if(snakeBody.size() > tail) {
+				snakeBody.remove(0);
+			}
+			
+			//check if eatting apple
+			if(head.equals(apple)) {
+				tail++;
+				score++;
+				//relocate apple after eatting
+				do{
+					apple.setLocation(random.nextInt(WIDTH / SCALE), random.nextInt(HEIGHT / SCALE));
+					overlapping = false;
+					for(Point point : snakeBody) {
+						if(apple.equals(point)) {
+							overlapping = true;
+						}
+					}
+				}while(overlapping); 
+			}
+		}
+		
+		//check for new high score
+		if(gameOver) {
+			if(score > highScore) {
+				String temp = "";
+				temp = Integer.toString(score);
+				//format string to match high score format
+				for(int i = temp.length(); i < 6; i++) {
+					temp = "0" + temp;
+				}
+				
+				//set new high score
+				setHighScore(temp);
+			}
+		}
+		
+		
+	}
+		
+	//check if head runs into tail
+	public boolean tailCheck(int x, int y) {
+		for(Point point : snakeBody) {
+			if(point.equals(new Point(x, y))){
+				return false;
+			}
+		}
+		
+		return true;
+	}
+	
+	
+	//MAIN CLASS
+	public static void main(String[] args) {
+		
+		snake = new Snake();
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//controls snake direction with WSAD
+	@Override
+	public void keyPressed(KeyEvent e) {
+		int i = e.getKeyCode();
+		if((i == KeyEvent.VK_A ) && direction != RIGHT) {
+			direction = LEFT;
+		}
+		if((i == KeyEvent.VK_D ) && direction != LEFT) {
+			direction = RIGHT;
+		}
+		if((i == KeyEvent.VK_S ) && direction != UP) {
+			direction = DOWN;
+		}
+		if((i == KeyEvent.VK_W ) && direction != DOWN) {
+			direction = UP;
+		}
+		
+		
+		//space to pause or restart the game
+		if(i == KeyEvent.VK_SPACE) {
+			if(gameOver) {
+				gameOver = false;
+				startGame();
+			}
+			else {
+				paused = !paused;
+			}
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	//get current high score from text file
+	public void getHighScore() {
+		File file = new File("C:\\Users\\Jonathan\\Desktop\\Java Files\\SnakeGame\\HighScore.txt");
+				
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			
+			//read file
+			String temp;
+			while((temp = br.readLine()) != null) {
+				highScoreString = temp;
+			}
+			
+			
+			br.close();
+		}
+		catch(IOException e) {
+			System.out.println(e + "\nFailed to open HighScore.txt");
+		}
+
+	}
+	
+	//write new high score to text file
+	public void setHighScore(String scoreString) {
+		File file = new File("C:\\Users\\Jonathan\\Desktop\\Java Files\\SnakeGame\\HighScore.txt");
+	
+		try {
+			FileWriter fout = new FileWriter(file);
+			
+			fout.write(scoreString);
+			
+			fout.close();
+		}
+		catch(IOException e) {
+			System.out.println(e + "\nFailed to write to HighScore.txt\n");
+		}
+	}
+	
+}
